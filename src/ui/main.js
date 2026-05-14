341ddebe0f1d1ba80f248c1e0443d83ce5dfd464
Islabre <islabre1426@gmail.com>
2026-05-14 21:38:24 +0700

    Feature: Add URLs UI
    - index.html: Adjusting footer buttons and sidebar main.
    - style.css: add-urls-container styling.
    - main.js:
        - Handle UI on "Add URLs" button click and sidebar button click.
        - Extract sidebar toggling UI to a function for reusability.

--- src/ui/main.js
+++ src/ui/main.js
@@ -6,17 +6,52 @@ window.addEventListener('pywebviewready', () => {
 function main() {
     const sidebarMain = document.getElementById('sidebar-main');
     const sidebarButton = document.getElementById('sidebar-button');
+    const addUrlsButton = document.getElementById('add-urls-button');
 
-    let extend = false;
+    let isSidebarExtended = false;
 
-    sidebarButton.addEventListener('click', () => {
+    sidebarButton.addEventListener('click', () => toggleSidebar(!isSidebarExtended));
+
+    addUrlsButton.addEventListener('click', () => {
+        const currentContent = sidebarMain.dataset.contentType;
+
+        if (isSidebarExtended && currentContent === 'add-urls') {
+            toggleSidebar(false);
+            return;
+        }
+
+        if (currentContent !== 'add-urls') {
+            renderAddUrlsUI();
+        }
+
+        toggleSidebar(true);
+    });
+
+    function renderAddUrlsUI() {
+        sidebarMain.dataset.contentType = 'add-urls';
+        sidebarMain.classList.add('add-urls-container');
+
+        sidebarMain.innerHTML = `
+        <header>
+            <span>Add URLs to Download</span>
+        </header>
+        <main>
+            <textarea id="urls"></textarea>
+        </main>
+        <footer>
+            <button id="inject-button">Inject!!</button>
+        </footer>
+        `;
+    }
+
+    function toggleSidebar(state) {
+        isSidebarExtended = state;
         const newWidth = Math.floor(window.innerWidth * 0.5);
 
-        extend = !extend;
-        pywebview.api.extendSidebar(extend);
+        pywebview.api.extendSidebar(isSidebarExtended);
 
-        sidebarMain.style.width = extend ? `${newWidth}px` : 'initial';
-        sidebarMain.style.display = extend ? 'block' : 'none';
-        sidebarButton.textContent = extend ? '<' : '>';
-    });
+        sidebarMain.style.width = isSidebarExtended ? `${newWidth}px` : 'initial';
+        sidebarMain.style.display = isSidebarExtended ? 'flex' : 'none';
+        sidebarButton.textContent = isSidebarExtended ? '<' : '>';
+    }
 }
