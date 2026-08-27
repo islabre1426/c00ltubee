047feebc94b8f9a9733aaee2d69a1d44d79ff293
Islabre <islabre1426@gmail.com>
2026-08-27 20:24:58 +0700

    Updating docs folder and upload script

--- upload.sh
+++ upload.sh
@@ -2,6 +2,16 @@
 
 set -eu
 
+if ! uname | grep -q -e MSYS -e Linux; then
+    echo "Please run under MSYS2, Linux or WSL to ensure compatibility."
+    exit 1
+fi
+
+if ! command -v rsync; then
+    echo "rsync required. Please install rsync first."
+    exit 1
+fi
+
 artifact="website/"
 remote_host="root@personal-server"
 name="c00ltubee"
