20ef7792ce8488f489d1b59c314d6f01b6fd42ed
Islabre <islabre1426@gmail.com>
2026-09-11 15:32:35 +0700

    Initial migration to yt-dlp binary

--- backend/config.py
+++ backend/config.py
@@ -4,55 +4,60 @@ from database.setting import setting_db
 from util.util import get_root_dir, current_os
 
 
-def get_downloader_opts():
+def get_downloader_opts_v2():
     vendor_dir = Path(get_root_dir(), 'vendor')
     qjs_exe = 'qjs.exe' if current_os == 'win32' else 'qjs'
+    yt_dlp_preset = [ 'mp3', 'aac', 'mp4', 'mkv' ]
 
     default_video_format = setting_db.get_value_by_name('default_video_format')
     default_audio_format = setting_db.get_value_by_name('default_audio_format')
 
-    best_audio_format_for_video_format = ''
+    opts = [
+        '--ignore-config',
 
-    match default_video_format:
-        case 'mp4':
-            best_audio_format_for_video_format = 'm4a'
+        '--paths', setting_db.get_value_by_name('download_location'),
+        '--output', setting_db.get_value_by_name('output_template'),
 
-    best_video = f'bestvideo*[ext={default_video_format}]'
-    best_audio = f'bestaudio'
+        '--no-playlist',
+        '--color', 'never',
 
-    if best_audio_format_for_video_format != '':
-        best_audio += f'[ext={best_audio_format_for_video_format}]'
+        '--ffmpeg-location', str(Path(vendor_dir, 'ffmpeg', current_os, 'bin')),
+        '--js-runtimes', 'quickjs:' + str(Path(vendor_dir, 'quickjs', current_os, qjs_exe)),
 
-    format = f'{best_video}+{best_audio}/best'
-
-    downloader_opts = {
-        'paths': {
-            'home': setting_db.get_value_by_name('download_location'),
-        },
-        'outtmpl': {
-            'default': setting_db.get_value_by_name('output_template'),
-        },
-        'noplaylist': True,
-        'color': 'never',
-        'ffmpeg_location': str(Path(vendor_dir, 'ffmpeg', current_os, 'bin')),
-        'js_runtimes': {
-            'quickjs': {
-                'path': str(Path(vendor_dir, 'quickjs', current_os, qjs_exe)),
-            }
-        },
-        'format': format,
-        'merge_output_format': default_video_format,
-    }
+        # For parsing info
+        '--print', r'[c00ltubee] Title: %(title)s',
+        '--progress-template', '[c00ltubee] Total Size: %(progress.total_bytes)s\tDownloaded: %(progress.downloaded_bytes)s',
+        '--no-simulate',
+        '--progress',
+        '--newline',
+        '--no-quiet',
+    ]
 
     if setting_db.get_value_by_name('audio_only') == 'true':
-        downloader_opts.update({
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': default_audio_format,
-            }],
-        })
+        if default_audio_format in yt_dlp_preset:
+            opts.extend([
+                '--preset-alias', default_audio_format,
+            ])
+        else:
+            opts.extend([
+                '--format', f'bestaudio[acodec^={default_audio_format}]/bestaudio/best',
+                '--extract-audio',
+                '--audio-format', default_audio_format,
+            ])
+    else:
+        if default_video_format in yt_dlp_preset:
+            opts.extend([
+                '--preset-alias', default_video_format,
+            ])
+        else:
+            opts.extend([
+                '--merge-output-format', default_video_format,
+                '--remux-video', default_video_format,
+            ])
 
-        downloader_opts.pop('merge_output_format')
-    
-    return downloader_opts
+    return opts
+
+
+
+def get_downloader_opts():
+    return get_downloader_opts_v2()
--- backend/downloader.py
+++ backend/downloader.py
@@ -1,18 +1,23 @@
 from datetime import datetime
 from pathlib import Path
 from queue import Queue
+import re
+import subprocess
 import threading
-
-from yt_dlp import YoutubeDL
 
 from backend.config import get_downloader_opts
 from database.download_history import download_history_db
-from util.util import get_app_data_location
+from util.util import get_app_data_location, get_root_dir, current_os
 
 
 download_queue = Queue()
 download_tasks = {}
 cancelling_tasks = set()
+
+yt_dlp_exe = 'yt-dlp.exe' if current_os == 'win32' else 'yt-dlp_linux'
+yt_dlp_path = str(Path(get_root_dir(), 'vendor', 'yt-dlp', current_os, yt_dlp_exe))
+
+waiting_title = 'Waiting...'
 
 
 class Logger:
@@ -22,7 +27,7 @@ class Logger:
         self.log_path = Path(get_app_data_location(), 'logs', today, f'{id}.log')
     
 
-    def write_log(self, msg: str):
+    def write(self, msg: str):
         log_path_parent = self.log_path.parent
 
         if not log_path_parent.exists():
@@ -31,18 +36,7 @@ class Logger:
         # Without encoding format, non-English message will look weird
         with self.log_path.open(mode = 'a', encoding = 'utf-8') as f:
             f.write(msg + '\n')
-    
 
-    def debug(self, msg: str):
-        self.write_log(msg)
-    
-
-    def warning(self, msg: str):
-        self.write_log(msg)
-
-    
-    def error(self, msg: str):
-        self.write_log(msg)
 
 
 def on_task_error(id: str):
@@ -85,81 +79,96 @@ def on_task_cancelled(id: str):
     cancelling_tasks.discard(id)
 
 
-def create_hook(id: str):
-    def hooks(d: dict):
-        if id in cancelling_tasks:
-            raise Exception(f'Task {id} cancelled by user')
 
-        status = d.get('status')
+def get_title(line: str) -> str:
+    matched = re.findall(r'^\[c00ltubee\] Title: (.*)$', line)
 
-        match status:
-            case 'downloading':
-                # Safely calculate percentage
-                total = d.get('total_bytes') or d.get('total_bytes_estimate') or 0
-                downloaded_bytes = d.get('downloaded_bytes', 0)
-                percentage = (downloaded_bytes / total * 100) if total > 0 else 0
+    if matched:
+        return matched[0]
+
+    return ''
+
+
+
+def get_size(line: str) -> dict:
+    matched = re.findall('^\[c00ltubee\] Total Size: (\d+)\tDownloaded: (\d+)$', line)
+
+    print(matched)
+
+    if matched:
+        return {
+            'total': matched[0][0],
+            'downloaded': matched[0][1],
+        }
+
+    return {}
+
+
+
+def download_video_v2(opts: list, id: str, url: str, logger: Logger):
+    download_tasks[id].update({
+        'status': 'starting',
+    })
+
+    cmd = [ yt_dlp_path, *opts, url ]
+
+    with subprocess.Popen(args = cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, text = True) as p:
+        for line in p.stdout:
+            if id in cancelling_tasks:
+                p.kill()
+
+            line = line.strip()
+
+            print(line)
+            logger.write(line)
+
+            if download_tasks[id]['status'] == 'starting' and download_tasks[id]['title'] == waiting_title:
+                title = get_title(line)
+
+                print('Retrieved title: ' + title)
+
+                if title:
+                    download_history_db.update_by_id(
+                        id,
+                        title,
+                        url,
+                        'working',
+                        str(logger.log_path),
+                    )
+
+                    download_tasks[id].update({
+                        'title': title,
+                        'progress': 0,
+                    })
+
+            size = get_size(line)
+
+            print('Retrieved size: ')
+            print(size)
+
+            if size:
+                total = int(size.get('total', 0))
+                downloaded = int(size.get('downloaded', 0))
+                percentage = (downloaded / total * 100) if total > 0 else 0
 
                 download_tasks[id].update({
                     'status': 'downloading',
                     'progress': round(percentage, 2),
                 })
-                
-            case 'finished':
-                on_task_success(id)
-            
-            case 'error':
-                on_task_error(id)
-                
-    
-    return hooks
+
+    if id in cancelling_tasks:
+        on_task_cancelled(id)
+
+    elif p.returncode != 0:
+        on_task_error(id)
+
+        raise subprocess.CalledProcessError(p.returncode, cmd)
+
+    else:
+        on_task_success(id)
 
 
-def download_video(opts: dict, id: str, url: str, log_file_path: str):
-    try:
-        with YoutubeDL(opts) as ydl:
-            info = ydl.extract_info(url, download = False)
-            info = ydl.sanitize_info(info)
-
-            title = info.get('title')
-            
-            output_filename = Path(ydl.prepare_filename(info))
-
-            if output_filename.exists():
-                on_task_success(id, title)
-                return
-            
-            # Support cancelling before the actual download
-            if id in cancelling_tasks:
-                on_task_cancelled(id)
-                return
-
-            download_history_db.update_by_id(
-                id,
-                title,
-                url,
-                'working',
-                log_file_path
-            )
-
-            download_tasks[id].update({
-                'status': 'starting',
-                'title': title,
-                'progress': 0,
-            })
-
-            ydl.download([url])
-
-    except Exception:
-        # Exception caused by intentional cancellation
-        if id in cancelling_tasks:
-            on_task_cancelled(id)
-            
-        else:
-            on_task_error(id)
-            raise
-
-
-def start_worker():
+def start_worker_v2():
     while True:
         if download_queue.empty():
             break
@@ -170,20 +179,20 @@ def start_worker():
 
         logger = Logger(id)
 
-        ydl_opts = {
-            **get_downloader_opts(),
-            'progress_hooks': [create_hook(id)],
-            'logger': logger,
-        }
+        opts = get_downloader_opts()
 
         task_process = threading.Thread(
-            target = download_video,
-            args = (ydl_opts, id, url, str(logger.log_path)),
+            target = download_video_v2,
+            args = (opts, id, url, logger),
             daemon = True,
             name = id,
         )
 
         task_process.start()
+
+
+def start_worker():
+    start_worker_v2()
 
 
 def add_task_to_queue(id: str, url: str):
@@ -197,13 +206,14 @@ def add_task_to_queue(id: str, url: str):
     except:
         download_history_db.add(
             id,
-            'Waiting...',
+            waiting_title,
             url,
             'queued',
         )
 
     download_tasks[id] = {
-        'status': 'queued'
+        'status': 'queued',
+        'title': waiting_title,
     }
 
 
